bd27879f2997182604d78dee40b79f1315d89be4
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-05 15:11:26 -0600

    Update Player.java

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -8,7 +8,7 @@ public class Player {
     /**
       * constructor that takes integer ID as argument
     */
-    public Player(int id, Token tok){
+    public Player(int id){
         this.id = id;
     }
 
