7a836988f7200384727dbd2dfe9765c8ee8f6741
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-05 16:03:29 -0600

    Create Railroad.java

---
+++ src/main/java/Model/Railroad.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Railroad extends Square{
+
+	public Railroad(int price, int position) {
+		super(price, position);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void perform(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
