d48f2d123bd077382fbc5a020a871802894385ae
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-05 15:58:40 -0600

    Create CommunityChest.java

---
+++ src/main/java/Model/CommunityChest.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class CommunityChest extends Card{
+
+	public CommunityChest(int price, int position) {
+		super(price, position);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void perform(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
