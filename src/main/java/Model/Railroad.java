3b8d556b7b1d084bf617534593e617053214343a
sahand <sahandri@rams.colostate.edu>
2018-04-05 19:12:04 -0600

    update Street.java, Railroad.java, and test files

--- src/main/java/Model/Railroad.java
+++ src/main/java/Model/Railroad.java
@@ -1,11 +1,20 @@
+/**
+ * Four railroads, players collect $25 rent if they own one railroad;
+ * $50 for two; $100 for three; $200 for all four.
+ *
+ */
 package Model;
 
 public class Railroad extends Square{
 
 	public Railroad(int price, int position) {
 		super(price, position);
-		// TODO Auto-generated constructor stub
+		if(!(position ==5 || position ==15 ||
+				position ==25 || position ==35)){
+			throw new IllegalArgumentException("Unknown position");
+		}
 	}
+
 
 	@Override
 	public void perform(Player player) {
