b7ae04828e5e75e59618513fa2ac8a76d33f9177
GoldenaArcher <lu.han9467@gmail.com>
2018-04-08 13:54:59 -0600

    wksps.xml

--- src/main/java/Model/Monopoly.java
+++ src/main/java/Model/Monopoly.java
@@ -2,10 +2,11 @@ package Model;
 
 public class Monopoly {
     // monopoly is the Main class for this assignment, used to initialize a monopoly game.
-    // 2 long variables are used to check the game time(i.e., 15 mins or 20 mins), if time exceeded the
-    // requirement, then game automatically ends, and whoever has the highest amount of money wins
-    private long initialTime, endTime;
+
 
     public static void main(String[] args) {
+        // 2 long variables are used to check the game time(i.e., 15 mins or 20 mins), if time exceeded the
+        // requirement, then game automatically ends, and whoever has the highest amount of money wins
+        long initialTime = System.currentTimeMillis(), endTime;
     }
 }
