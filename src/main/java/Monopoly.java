1ccbfc1ab361eaa1d00e4d73ff5f7ad9c19224af
GoldenaArcher <lu.han9467@gmail.com>
2018-04-08 23:12:35 -0600

    add several changes in the panel, move driver class out of model, and update maven setting

--- src/main/java/Monopoly.java
+++ src/main/java/Monopoly.java
@@ -1,4 +1,7 @@
-package Model;
+import View.MonopolyPanel;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Monopoly {
     // monopoly is the Main class for this assignment, used to initialize a monopoly game.
@@ -8,5 +11,14 @@ public class Monopoly {
         // 2 long variables are used to check the game time(i.e., 15 mins or 20 mins), if time exceeded the
         // requirement, then game automatically ends, and whoever has the highest amount of money wins
         long initialTime = System.currentTimeMillis(), endTime;
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        MonopolyPanel monopolyPanel = new MonopolyPanel();
+        monopolyPanel.setVisible(true);
+        monopolyPanel.setExtendedState(Frame.MAXIMIZED_BOTH);
     }
 }
---
+++ src/main/java/View/MonopolyPanel.java
@@ -0,0 +1,27 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MonopolyPanel extends JFrame implements ActionListener {
+    private Container container = getContentPane();
+    private JPanel jPanel[] = new JPanel[4];
+
+    public MonopolyPanel(){
+        //        As menu is not needed for now, so the menu setup is left out
+//        set up the layout for the container
+        container.setLayout(new BorderLayout());
+
+//        initialize JPanel with 4 sections of area
+        for (int i = 0; i < 4; i++) {
+            jPanel[i] = new JPanel();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
