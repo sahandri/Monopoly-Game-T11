07cc87b79620c7f3cb2d52679e1d9a2068fe362f
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-13 12:12:58 -0600

    Update TestSquare.java

--- src/test/java/Model/TestSquare.java
+++ src/test/java/Model/TestSquare.java
@@ -1,4 +1,19 @@
 package Model;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestSquare {
+	
+	@Before
+	public void initialize(){
+		
+	}
+	
+	@Test
+	public void test(){
+		
+	}
+	
 }
