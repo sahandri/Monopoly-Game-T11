04bfcac8ded5322588cc20c5963620a0eb9f7ff5
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-13 12:10:37 -0600

    Update TestMonopoly.java

--- src/test/java/Model/TestMonopoly.java
+++ src/test/java/Model/TestMonopoly.java
@@ -1,4 +1,17 @@
 package Model;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestMonopoly {
+	@Before
+	public void initialize(){
+		
+	}
+	
+	@Test
+	public void testMethod(){
+		
+	}
 }
