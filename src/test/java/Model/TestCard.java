5dce01a6804442a708c6f5a3015b83751c8b8561
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-13 12:06:24 -0600

    Update TestCard.java

--- src/test/java/Model/TestCard.java
+++ src/test/java/Model/TestCard.java
@@ -1,4 +1,21 @@
 package Model;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestCard {
+	private ChanceCard chance;
+	private CommunityChest com_chest;
+	
+	@Before
+	public void initialize(){
+		chance = new ChanceCard(0,0);
+		com_chest = new CommunityChest(0,0);
+	}
+	
+	@Test
+	public void testMethods(){
+		
+	}
 }
