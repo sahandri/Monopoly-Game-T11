d529316fa32e494fd856ae9fcc8a0e3475bf3264
ejrober <e.j.roberts14@gmail.com>
2018-04-13 10:18:14 -0600

    Utilities and TestUtilities implementations.

--- src/main/java/Model/Utilities.java
+++ src/main/java/Model/Utilities.java
@@ -1,5 +1,7 @@
 package Model;
 
+import src.main.java.Model.Tax.taxes;
+
 public class Utilities extends Square {
 
     /*
@@ -9,11 +11,69 @@ public class Utilities extends Square {
      spaces, she lands her token on Electric Company, which is owned by Sarah. The dice roll (7) is multiplied by
      4, for a total of 28; Mary now owes Sarah $28 rent.
     */
+	
+	public enum utilities{
+		WATER_WORKS,
+		ELECTRICITY_COMPANY
+	}
+	
+	private utilities utility;
+	
     public Utilities(int price, int position) {
-        super(price, position);
+    	super(price, position);
+		try {
+			setUtility(position);
+		}catch(IllegalArgumentException e){
+			System.err.println("invalid position"+e.getMessage());
+		}
     }
 
-    public void perform(Player player, Board board) {
+    private void setUtility(int position) {
+    	if(position == 12) {
+			utility = utilities.ELECTRICITY_COMPANY;
+		}
+		else if(position == 28) {
+			utility = utilities.WATER_WORKS;
+		}
+		else {
+			throw new IllegalArgumentException("Unknown position");
+		}
+		
+	}
+    
+    public String toString(){
+		switch(this.utility){
+			case ELECTRICITY_COMPANY: return "Electricity Company";
+			case WATER_WORKS: return "Water Works";
+			default: return "Type not defined.";
+		}
+	}
 
+
+	public void perform(Player player, Board board) {
+		switch(this.utility){
+		case ELECTRICITY_COMPANY:
+			if(player.getDecision() == true){
+				player.getMoney().sbustractMoney(150);
+			}
+			/**else if(this.utility is owned){
+			 * payRent(player, board);
+			 * }
+			 */
+			break;
+		case WATER_WORKS:
+			if(player.getDecision() == true){
+				player.getMoney().sbustractMoney(150);
+			}
+			/**else if(this.utility is owned){
+			 * payRent(player, board);
+			 * }
+			 */
+			break;
+		}
     }
+	
+	public void payRent(Player player, Board board) {
+		player.getMoney().sbustractMoney(4 * board.roll());
+	}
 }
