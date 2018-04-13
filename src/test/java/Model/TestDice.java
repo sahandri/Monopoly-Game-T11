a05e9273523c178b27d9c63ce2effe1696590fe4
emadal95 <31264880+emadal95@users.noreply.github.com>
2018-04-13 12:08:29 -0600

    Update TestDice.java

--- src/test/java/Model/TestDice.java
+++ src/test/java/Model/TestDice.java
@@ -1,4 +1,20 @@
 package Model;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestDice {
+	private Dice dice;
+	
+	@Before 
+	public void initialize(){
+		dice = new Dice();
+	}
+	
+	@Test
+	public void testRoll(){
+		int n = dice.roll();
+		assertTrue((1<=n)&&(n<=6));
+	}
 }
